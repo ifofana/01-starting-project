32b742cfdca4113cc79b79a9436e47a503eb8377
IFof <ifofana75@gmail.com>
2025-12-01 00:25:07 -0500

    Initial commit - Angular 20 project

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,46 @@
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
+import { type User } from './user.model';
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
+  // @Input({required: true}) user!: {
+  //   id: string;
+  //   avatar: string;
+  //   name: string;
+  // };
+
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string; //This will marks this object as settable from outside the component
+  // @Input({required: true}) name!: string;
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  @Output() select = new EventEmitter<string>();
+  //select = output<string>(); //This is relatively new syntax for output event emitter
+
+  // imagePath = computed(() => {
+  //   return `assets/users/` + this.avatar();
+  // });
+
+  get imagePath() {
+    return `assets/users/` + this.user.avatar;//this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(/*this.id*/ this.user.id);
+  }
+}
